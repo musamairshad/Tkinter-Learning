43da115c442f20faea219be418d21284b687b8ca
Muhammad Usama <imusamairshad@gmail.com>
2023-06-07 13:02:45 +0500

    Tutorial#4.py is added.

---
+++ Tutorial#4.py
@@ -0,0 +1,29 @@
+from tkinter import *
+
+# usamairshad_root = Tk()
+
+# # Width x Height
+# usamairshad_root.geometry("644x434")
+
+# # width, height
+# usamairshad_root.minsize(300, 100)
+
+# # width, height
+# usamairshad_root.maxsize(1200, 988)
+
+# usama = Label(text="Usama is a good boy and this is his GUI.")
+# usama.pack()
+
+# usamairshad_root.mainloop()
+
+
+# PyCharm Sample Window
+
+usamairshad_root = Tk()
+
+usamairshad_root.geometry("733x434")
+
+usama = Label(text="Welcome to PyCharm!")
+usama.pack()
+
+usamairshad_root.mainloop()
